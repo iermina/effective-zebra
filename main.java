ebc3a5452bc3509a113813d3eb34a974b3cb242a
iermina <167538000+iermina@users.noreply.github.com>
2024-09-18 20:15:51 +0000

    Pending changes exported from your codespace

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main 
+{
+
+    public static void main (String[] args)
+    {
+        String a = "AP CSA";
+        int b = 2;
+        double t = 15;
+        boolean c = false;
+        b = b - 1;
+        final int g = 20;
+        System.out.print(g);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(t);
+        System.out.println(c);
+
+    }
+}
